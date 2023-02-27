733b2ae053428e7ead46f0bd5c454bc4ae8b7c49
Boginskiy Bogdan <bboginskiy@rockitsoft.ru>
2023-02-27 22:50:21 +0300

    Chapter I. Adding controller with home view. Init commit.

---
+++ Chapter 1/src/test/java/ru/bossones/firstchapter/Chapter1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bossones.firstchapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
